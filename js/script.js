94716576db0a5b6702d3a556e441f3a40046cfdf
Pawel Jurecki <jurecki.pawel@gmail.com>
2021-08-23 11:44:25 +0200

    Add navigation with js function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,37 @@
 "use strict";
 
+const titleClickHandler = function (event) {
+  const clickedElement = this;
+  event.preventDefault();
+
+  /* remove class 'active' from all article links  */
+
+  const activeLinks = document.querySelectorAll(".titles a.active");
+
+  for (let activeLink of activeLinks) {
+    activeLink.classList.remove("active");
+  }
+
+  /* add class 'active' to the clicked link */
+  clickedElement.classList.add("active");
+
+  /* remove class 'active' from all articles */
+  const activeAricles = document.querySelectorAll(".post.active");
+  for (let activeAricle of activeAricles) {
+    activeAricle.classList.remove("active");
+  }
+  /* get 'href' attribute from the clicked link */
+  const articleID = clickedElement.getAttribute("href").substring(1);
+
+  /* find the correct article using the selector (value of 'href' attribute) */
+  const article = document.getElementById(articleID);
+
+  /* add class 'active' to the correct article */
+  article.classList.add("active");
+};
+
 const links = document.querySelectorAll(".titles a");
 
 for (let link of links) {
-  console.log(link);
+  link.addEventListener("click", titleClickHandler);
 }
