58fb9a0bd2639dbfa214acd31dc4b887b6faf078
Pawel Jurecki <jurecki.pawel@gmail.com>
2021-08-23 12:16:23 +0200

    Add generateTitleLinks function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,36 @@
 "use strict";
 
+const optArticleSelector = ".post",
+  optTitleSelector = ".post-title",
+  optTitleListSelector = ".titles";
+
+function generateTitleLinks() {
+  /* remove contents of titleList */
+  const listOfTitles = document.querySelector(optTitleListSelector);
+  listOfTitles.innerHTML = "";
+
+  /* for each article */
+  const articles = [...document.querySelectorAll(optArticleSelector)];
+
+  for (let article of articles) {
+    /* get the article id */
+    const articleID = article.getAttribute("id");
+
+    /* find the title element */
+    /* get the title from the title element */
+    const titleOfArticle = article.querySelector(optTitleSelector).textContent;
+
+    /* create HTML of the link */
+    const li = document.createElement("li");
+    li.innerHTML = `<a href="#${articleID}"><span>${titleOfArticle}</span></a>`;
+    listOfTitles.appendChild(li);
+  }
+}
+
+generateTitleLinks();
+
+const links = [...document.querySelectorAll(".titles a")];
+
 const titleClickHandler = function (event) {
   const clickedElement = this;
   event.preventDefault();
@@ -29,8 +60,6 @@ const titleClickHandler = function (event) {
   /* add class 'active' to the correct article */
   article.classList.add("active");
 };
-
-const links = document.querySelectorAll(".titles a");
 
 for (let link of links) {
   link.addEventListener("click", titleClickHandler);
